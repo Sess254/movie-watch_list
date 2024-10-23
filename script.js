245e881f94d3aa85fd1539eac2b04ea565badecf
Sess254 <wamaritemoses@gmail.com>
2024-10-23 18:12:26 +0300

    mobie layout done

---
+++ script.js
@@ -0,0 +1,15 @@
+const searchBtn = document.getElementById('search-btn')
+
+searchBtn.addEventListener('click', getMovie)
+
+
+
+
+async function getMovie() {
+    const res = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=33ae5430&t=blade runner 2049")
+    const data = await res.json()
+    
+    
+
+
+}
