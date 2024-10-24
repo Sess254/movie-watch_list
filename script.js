445fa52bb1ae41013ff433347cfc8e5500bc2339
Sess254 <wamaritemoses@gmail.com>
2024-10-24 19:04:58 +0300

    minor bug fix for mobie layout

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ searchBtn.addEventListener('click', getMovie)
 async function getMovie() {
     const res = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=33ae5430&t=blade runner 2049")
     const data = await res.json()
-    
-    
+    console.log(data)
 
 
 }
